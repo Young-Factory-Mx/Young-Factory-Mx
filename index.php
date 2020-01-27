c62d32880f5a96262321350961acf4d2127cdac5
Epsilon <youngfactorymx@gmail.com>
2020-01-27 05:22:07 -0600

    Primer commit d emi sitio web

---
+++ index.php
@@ -0,0 +1,437 @@
+<!DOCTYPE html>
+<html lang="es">
+<!--Comienza la cabecera -->
+<head>
+  <link rel="stylesheet" href="estilos.css">
+  <style>
+@import url('https://fonts.googleapis.com/css?family=Ubuntu:700&display=swap');
+</style>
+  <meta charset="utf-8">
+  <title>Young Factory MX - Publicidad Marketing & Sistemas Computacionales</title>
+  <link href="https://fonts.googleapis.com/css?family=Poppins$display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="owl/owl.carousel.min.css">
+  <link rel="stylesheet" href="owl/owl.theme.default.min.css">
+  <link rel="icon" href="IMAGENES/YOUNG FACTORY MX PAGINAS WEB APLICACIONES SOFTWARE DISEÑO DE MARCA.ico"  alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+  <meta name="viewport"
+  content="width=device-width,
+  user-scalable=no">
+ 
+</head>
+
+
+<STYLE>A {text-decoration: none;} </STYLE>
+<!--Termina la cabecera-->
+<body>
+
+
+<header>
+
+<div class="menu">
+<div class="contenedor" id="contenedor-menu">
+  <a href="www.youngfactorymx.com">
+  <p class= "logo">Young Factory Mx</p></a>
+  <img class="menu-icon" src="IMAGENES/menu.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+  <nav>
+   <ul>
+     <li> <a href="#about">Nosotros</a></li>
+     <li> <a href="#servicios">Servicios</a></li>
+     <li> <a href="#efectuados">Trabajos</a></li>
+     <li> <a href="#por-que-trabajar">¿Por qué con Young Factory?</a></li>
+     <li> <a href ="#blog">Descarga</a></li>
+     <li> <a href ="#contacto">Contacto</a></li>
+   </ul>
+  </nav>
+</div>
+</div>
+
+<div class="contenedor">
+  <div class="contenedor-titulo">
+    <div class="titulo">
+      <img src="IMAGENES/Young Factory pagina web marketing publicidad diseño negocios 1.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+      <h2>Young Factory Mx</h2>
+      <h3>Publicidad Marketing & Sistemas Computacionales</h3>
+      <a href="#cotizar">CUPON PROMOCIONAL</a>
+    </div>
+  </div>
+</div>
+</header>
+
+<main>
+  <section id="about">
+    <div class="contenedor">
+      <h4>Nosotros</h4>
+      <div class="contenido">
+        <div class="about-text">
+          <p>Comprometidos con el crecimiento de nuestros clientes, ofrecemos servicios para el creciemiento y posicionamiento on-line de tu negocio, PYME, empresa o local comercial. Trabajando desde el diseño de la imagen, diseño de marca/identidad, campañas publicitarias, tarjeta de negocios codificada etc... Hasta tu propia PÁGINA WEB PROFESIONAL!
+          <br>Desde el 2016 somos profesionales en la materia certificados por Google y Facebook for Business, siempre en constante capacitación para mantener ideas frescas y entusiastas con las que te apoyaremos al crecimiento de tu negocio.</br> </p>
+
+<a href="#efectuados">Ver Trabajos</a>
+<div class="about-img"><img src="IMAGENES/YOUNG FACTORY MX PUBLICIDAD MARKETING PAGINAS WEB NEGOCIOS.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios"></div>
+         </div>
+        </div>
+      </div>
+    </section>
+
+      <section id="servicios">
+        <div class="contenedor">
+          <h6>Servicios</h6>
+            <div class="contenedor-servicios">
+<!--1 BRANDING-->
+              <div class="servicio cafe">
+                <h5>Diseño de Imagen Corporativa</h5>
+                <p>Construimos tu marca (nombre, logo, paleta de colores e identidad corporativa de tu negocio) ya sea que se construya tu marca desde cero o que ya cuentes con tu marca pero deseas actualizarte, refrescar la imagen, cambiar el nombre pero conservar el concepto, rediseño del logo etc... nosotros nos encargamos de plasmar tu idea.
+                </p>
+              </div>
+<!--2 PAGINA WEB PROFESIONAL-->
+              <div class="servicio cafe">
+                <h5>Página Web Profesional Personalizada</h5>
+                <p>
+                  Creamos sitios web atractivos y completamente funcionales de velocidad de carga alta para que tu publico tenga una mejor experiencia. Tu sitio estará adaptado con la mejor tecnología para que los usuarios puedan consultarlo desde cualquier dispositivo y ademas contará con un certificado SSL de seguridad y alta en Google Maps.
+                </p>
+              </div>
+<!--3 PLAN DE MARKETING-->
+              <div class="servicio cafe">
+                <h5>Plan de Marketing Digital</h5>
+                <p>Creamos estrategias de marketing digital y campañas publicitarias efectivas para atraer clientes potenciales empleando las herramientas adecuadas de análisis de datos para generar un embudo de ventas digital, creamos trafico hacia tu sitio web, diseño de publicidad de alto impacto, anuncios 30% mas efectivos y segmentación de tu publico.</p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+<!--SECCION TRABAJOS EFECTUADOS(CARRUSEL)-->
+        <section id="efectuados">
+          <div class="contenedor">
+            <h7>Trabajos Efectuados</h7>
+            <div class="owl-carousel owl-theme">
+<!--Young Factory MX -1 ELECTRONICA GARCIA-->
+          <div class="item">
+          <a href="www.google.com">
+          <img src="IMAGENES/YOUNG FACTORY MX DISEÑO DE MARCA ELECTRONICA GARCIA PUBLICIDAD NEGOCIOS.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+          <p>Diseño de Marca e Identidad, Sitio Web Etc...</p>
+        </a>
+        </div>
+<!--Young Factory MX -2 DAVID PRO-->
+<div class="item">
+<a href="https://www.facebook.com/youngfactorymx/">
+<img src="IMAGENES/young-factory-mx-diseño-grafico-publicidad-negocios-marketing-branding.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+<p>Diseño Gráfico Original, Personalizado</p>
+</a>
+</div>
+<!--Young Factory MX -3 PASTELERÍA COCO-->
+<div class="item">
+<a href="https://www.facebook.com/youngfactorymx/">
+<img src="IMAGENES/young factory mx PAGINA WEB DISEÑO DE MARCA LOGO MARKETING PUBLICIDAD NEGOCIOS.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+<p>Creación de Marca e Identidad, Sitio Web Etc...</p>
+</a>
+</div>
+<!--Young Factory MX -4 BAMBAMBARBER-->
+<div class="item">
+<a href="https://www.facebook.com/youngfactorymx/">
+<img src="IMAGENES/YOUNG-FACTORY-MX-DISEÑO-DE-MARCA--TACOS-EL-PAPI-PUBLICIDAD-NEGOCIOS-LOGO-MARKETING-PAYASO-BARBER.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+<p>Diseño de Marca y Tarjetas de Negocio</p>
+</a>
+</div>
+<!--Young Factory MX -5 TACOS EL PAPI-->
+<div class="item">
+<a href="https://www.facebook.com/youngfactorymx/">
+<img src="IMAGENES/YOUNG FACTORY MX DISEÑO DE MARCA  TACOS EL PAPI PUBLICIDAD NEGOCIOS LOGO MARKETING.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+<p>Diseño Personalizado de Marca, Sitio Web Etc...</p>
+</a>
+</div>
+<!--Young Factory MX -6 INOXIDABLES ORIENTE-->
+<div class="item">
+<a href="https://www.facebook.com/youngfactorymx/">
+<img src="IMAGENES/young-factory-mx-diseño-de-marca-logo-identidad-publicidad-negocios-marketing-branding.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+<p>Creación de Marca e Identidad, Sitio Web Etc...</p>
+</a>
+</div>
+<!--Young Factory MX -7 LA BARRA DE GESTO-->
+<div class="item">
+<a href="https://www.facebook.com/youngfactorymx/">
+<img src="IMAGENES/young factory mx diseño grafico publicidad negocios marketing branding CERVEZA.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+<p>Creación de Marca e Identidad, Sitio Web Etc...</p>
+</a>
+</div>
+<!--Young Factory MX -8 CHUBBY-->
+<div class="item">
+<a href="https://www.facebook.com/youngfactorymx/">
+<img src="IMAGENES/YOUNG-FACTORY-MX-DISEÑO-DE-MARCA--TACOS-EL-PAPI-PUBLICIDAD-NEGOCIOS-LOGO-MARKETING-PAYASO-CIRCO.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+<p>Publicidad, Diseño Gráfico, Tarjetas</p>
+</a>
+</div>
+<!--Young Factory MX -10 TARJETAS-->
+<div class="item">
+<a href="https://www.facebook.com/youngfactorymx/">
+<img src="IMAGENES/YOUNG FACTORY MX DISEÑO DE MARCA ELECTRONICA GARCIA PUBLICIDAD NEGOCIOS tarjetas.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+<p>Recreación de Marca-Identidad, Tarjetas Etc...</p>
+</a>
+</div>
+
+<!--Young Factory MX -11 MAS GRADUACIONES-->
+<div class="item">
+<a href="https://www.facebook.com/youngfactorymx/">
+<img src="IMAGENES/YOUNG-FACTORY-MX-DISEÑO-DE-MARCA-ELECTRONICA-GARCIA-PUBLICIDAD-NEGOCIOS-FB-W.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+<p>Rehabilitado de logotipo establecido</p>
+</a>
+</div>
+
+<!--Young Factory MX -12 LUNAMOBILE-->
+<div class="item">
+<a href="https://www.facebook.com/youngfactorymx/">
+<img src="IMAGENES/YOUNG-FACTORY-MX-DISEÑO-DE-MARCA-ELECTRONICA-GARCIA-PUBLICIDAD-NEGOCIOS-Y.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+<p>Recreación de Marca-Identidad Sitio Web</p>
+</a>
+</div>
+
+<!--Young Factory MX -YOUNGFACTORY-->
+<div class="item">
+<a href="https://www.facebook.com/youngfactorymx/">
+<img src="IMAGENES/YOUNG-FACTORY-MX-DISEÑO-DE-MARCA-ELECTRONICA-GARCIA-PUBLICIDAD-NEGOCIOS-FB.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+<p>For Business |For Enterprise| For Education</p>
+</a>
+</div>
+
+
+</div>
+</div>
+
+</section>
+<!--Young Factory MX - SECCIÓN ¿POR QUE TRABAJAR CON NOSOTROS?-->
+<section id="por-que-trabajar">
+  <div class="contenedor">
+    <h7>¿Por que trabajar con nostros?</h7>
+    <div class="contenedor-trabajar">
+      <div class="item">
+      <ul>
+        <li><img src="IMAGENES/YOUNG FACTORY MX PUBLICIDAD MARKETING PAGINAS WEB NEGOCIOS f.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios"></li>
+        <li>Originalidad y Presencia en tu Imagen</li>
+        <li>Muestra una imagen original de tu negocio e identidad propia, imagen de calidad y profecionalismo, convence a tus clientes a primera vista, que tu imagen transmita la esencia de tu negocio, seriedad y confianza.</li>
+      </ul>
+      </div>
+
+      <div class="item">
+      <ul>
+        <li><img src="IMAGENES/YOUNG FACTORY MX PUBLICIDAD MARKETING PAGINAS WEB NEGOCIOS fa.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios"></li>
+        <li>Posisionamiento y Crecimiento Asegurado</li>
+        <li>Incrementa clientes potenciales y resultados, empleamos estrategias profesionales de crecimiento en las que ocupamos diversas metodologias cuantitativas y cualitativas colocando tu negocio en un nivel competitivo. </li>
+      </ul>
+      </div>
+
+      <div class="item">
+      <ul>
+        <li><img src="IMAGENES/YOUNG FACTORY MX PUBLICIDAD MARKETING PAGINAS WEB NEGOCIOS fac.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios"></li>
+        <li>Cuida tu Dinero/Asegura su retorno</li>
+        <li>Obten resultados de un servicio profesional por una inversión accesible y de rapido retorno, rompe el paradigma de que se requiere de un capital enorme para crecer como empresa. !Haz que tus clientes te encuentren! Con un buen manejo de redes y tu nuevo sitio web.</li>
+      </ul>
+      </div>
+
+    </div>
+  </div>
+</section>
+<!--Young Factory MX - SECCIÓN BLOG1-->
+<section id="blog">
+  <div class="contenedor">
+    <h6>Descargas</h6>
+    <div class="contenido">
+
+      <div class="blog-single">
+        <div class="blog-img">
+        <img src="IMAGENES/YOUNG FACTORY MX PUBLICIDAD MARKETING PAGINAS WEB NEGOCIOS emprendedor.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+      </div>
+      <div class="blog-box-content">
+         <h5>Empieza tu mismo a hacer un plan de negocios</h5>
+         <p>Para comenzar en los negocios hay aspectos fundamentales que debes tomar en cuenta, con este libro aprenderas paso a paso como comenzar la proyección de tu negocio.</p>
+         <a href="https://drive.google.com/file/d/1qqQDZ2VBm0PYWtgvCHHAMuwzVm48T12u/view">Descargar →</a>
+</div>
+</div>
+
+<div class="blog-single">
+  <div class="blog-img">
+  <img src="IMAGENES/YOUNG FACTORY MX PUBLICIDAD MARKETING PAGINAS WEB NEGOCIOS emprendedor CV.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+</div>
+<div class="blog-box-content">
+   <h5>Plantilla para un CV excepcional</h5>
+   <p>Un CV efectivo es aquel que incluye información relevante pero cuidadosamente seleccionada y estructurada, para ello elaboramos esta plantilla utilizala y olvidate de errar.</p>
+   <a href="">Descargar →</a>
+</div>
+</div>
+
+<div class="blog-single">
+  <div class="blog-img">
+  <img src="IMAGENES/YOUNG FACTORY MX PUBLICIDAD MARKETING PAGINAS WEB NEGOCIOS sitio web.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+</div>
+<div class="blog-box-content">
+   <h5>Formaliza tu negocio en linea</h5>
+   <p>Si no estas en internet, entonces no estas! creamos sitios web profesionales atractivos y efectivos para formalizar de una vez por todas tu negocio en internet, ¿Que esperas?</p>
+   <a href="#cotizar">DESCUENTO →</a>
+</div>
+</div>
+    </div>
+      </div>
+</section>
+<!--Young Factory MX - SECCIÓN BLOG2-->
+<section id="blog2">
+  <div class="contenedor">
+    <h8></h8>
+    <div class="contenido">
+
+<div class="blog-single">
+ <div class="blog-img">
+ <img src="IMAGENES/YOUNG FACTORY MX PUBLICIDAD MARKETING PAGINAS WEB NEGOCIOS INVENTARIO.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+ </div>
+ <div class="blog-box-content">
+    <h5>Lleva el control de tus entradas y salidas</h5>
+    <p>LLevar inventario de los productos terminados o de la materia prima para los procesos de producción es algo que debes entender, con el siguiente libro aprenderas lo necesario.</p>
+    <a href="https://drive.google.com/file/d/1hBoF-6wflyw7h-7AneDc2AcjDI_ZwLTI/view?usp=sharing">Descargar → </a>
+
+</div>
+</div>
+
+<div class="blog-single">
+  <div class="blog-img">
+  <img src="IMAGENES/YOUNG FACTORY MX PUBLICIDAD MARKETING PAGINAS WEB NEGOCIOS MARKETING.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+</div>
+<div class="blog-box-content">
+   <h5>Estrategia de Marketing Digital</h5>
+   <p>Creamos campañas publicitarias efectivas para atraer clientes potenciales, empleando las herramientas adecuadas de análisis de datos para generar un embudo de ventas efectivo.</p>
+   <a href="#cotizar">DESCUENTO →</a>
+</div>
+</div>
+
+<div class="blog-single">
+  <div class="blog-img">
+  <img src="IMAGENES/YOUNG FACTORY MX PUBLICIDAD MARKETING PAGINAS WEB NEGOCIOS CONTABILIDAD.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+</div>
+<div class="blog-box-content">
+   <h5>Aprende contabilidad para tu negocio</h5>
+   <p>Aunque en ocaciones quien lleva la contabilidad de tu negocio no seas tú, necesitas saber de que manera el dinero hace funcionar el negocio en genearl, descarga este curso de contabilidad.</p>
+   <a href="https://drive.google.com/file/d/11-k3JGa2Y4u1zADJ5JAyD1oaEcFblo-S/view?usp=sharing">Descargar →</a>
+</div>
+</div>
+</div>
+</div>
+</section>
+<!--Young Factory MX - COTIZAR-->
+<section id="cotizar">
+  <div class="contenedor">
+    <h8>¿Deseas formar un paquete con varios de nuestros servicios a un super precio?</h8>
+    <a href="https://api.whatsapp.com/send?phone=5215574751418">COTIZAR MENOS 35% PROMOCIONAL</a>
+  </div>
+</section>
+<!--Young Factory MX - CONTACTANOS-->
+
+<section id="contacto">
+ 
+<div class="contenedor-contacto"> 
+   
+  <div class="contacto">
+    <h8>Click en el ícono de Whatssap para ponernos en Contacto!</h8> </div>
+    
+  
+ 
+<!--Young Factory MX - información-->
+  <div class="contacto-info">
+
+    <div class="mail"> <img src="IMAGENES/mail-icon2.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">agencia.digital@youngfactorymx.com</div>
+    <div class="whatsapp"> <img src="IMAGENES/whatsapp-icon2.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">+521-5574751418</div>
+    </div>
+
+
+    </div>
+</section>
+</main>
+
+<footer>
+  <div class="contenedor">
+    <div class="redes">
+      <a href="">
+      <img src="IMAGENES/lin.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios"></a>
+      <a href="https://www.facebook.com/KennolyRamos">
+      <img src="IMAGENES/fab.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios"></a>
+      <a href="https://www.facebook.com/KennolyRamos">
+      <img src="IMAGENES/ins.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios"></a>
+    </div>
+    <div class="parrafo">
+      <p>©️Young Factory Mx 2019</p> </div>
+    <div class="parrafo2">
+      <p>CEO/Consultor Kevin García Ramos | Ciudad de México | Todos los derechos reservados</p>
+    </div>
+  </div>
+</footer>
+
+
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+<script src="owl/owl.carousel.min.js"></script>
+
+<script>
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+</script>
+
+<script>
+
+$(document).ready(function(){
+
+$(window).scroll(function(){
+ scroll = $(window).scrollTop();
+  if (scroll > 100) {
+    $('.menu').css({"position":"fixed"});
+    $('.menu').css({"width":"100%"});
+    $('.menu').css({"height":"14%"});
+    $('.menu').css({"top":"0"});
+    $('.menu').css({"background":"#211717"});
+    $('.menu a').css({"color":"#FFD700"});
+    $('.menu').css({"box-shadow":"rgb(0, 0, 0, 0.25) 6px 2px 2px"});
+    $('.menu').css({"z-index":"100"});
+  } else {
+//cuando no hay scroll/ cuando el scroll es para arriva y no para abajo//
+   $('.menu').css({"position":"relative"});
+   $('.menu').css({"background":"transparent"});
+   $('.menu').css({"box-shadow":"rgb(0, 0, 0, 0) 0px 0px 0px"});
+   $('.menu a').css({"color":"#FFF"});
+        }
+})
+
+$('.menu-icon').click(function(){
+  $('header nav').slideToggle();
+})
+
+})
+</script>
+
+<script>
+$(document).ready(function()){
+  $(window).scroll(function){
+    var barra = $(window).scrollTop();
+    var posicion = barra *0.40;
+    $('body').css({
+      'background-position'; '0 -' + posicion + 'px'
+    });
+  });
+});
+</script>
+
+<a id="whatsapp1"
+href="https://api.whatsapp.com/send?phone=5215574751418">
+<img src="IMAGENES/wats.png" alt="Young factory agencia publicitaria marketing cdmx ventas negocios">
+</a>
+
+</body>
+</html>
